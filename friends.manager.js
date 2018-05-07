44f2f6687ba788ba7f8f9c174af60fc9cf9009c8
Casey Spinner <33559400+caseyspinner@users.noreply.github.com>
2018-05-07 16:18:09 -0700

    Update friends.manager.js

--- friends.manager.js
+++ friends.manager.js
@@ -12,20 +12,16 @@ module.exports = {
 function acceptRequest(requestId) {
   return friendsService.acceptRequest(requestId).then(request => {
     const promises = [
-      new Promise((resolve, reject) => {
         friendsService
           .addToFriendsList(request.targetUserId, request.requestorUserId)
           .then(data => {
             resolve(data);
-          });
-      }),
-      new Promise((resolve, reject) => {
+          }),
         friendsService
           .addToFriendsList(request.requestorUserId, request.targetUserId)
           .then(data => {
             resolve(data);
-          });
-      })
+          })
     ];
     return Promise.all(promises).then(response => {
       Promise.resolve();
